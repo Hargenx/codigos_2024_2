a3e2c65f3193b51999911eb428f8b48536d5115d
Raphael Mauricio Sanches de Jesus <55360548+Hargenx@users.noreply.github.com>
2024-10-09 20:49:28 -0300

    exemplos e correções de simulado

---
+++ python/paradgmas/simulado1/questao06.py
@@ -0,0 +1,8 @@
+numero = 1
+soma = 0
+
+while(numero!=0):
+    numero = float(input('informe o numero(Caso seja 0 finaliza): '))
+    soma = numero + soma
+
+print('O valor da soma dos valores é: ', soma)
