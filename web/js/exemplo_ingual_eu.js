fc8ca09bf27b8e733e602c10ebeb91cd88e03d80
Raphael Mauricio Sanches de Jesus <55360548+Hargenx@users.noreply.github.com>
2024-10-11 08:44:34 -0300

    exemplo

---
+++ web/js/exemplo_ingual_eu.js
@@ -0,0 +1,16 @@
+let a = 5;
+let b = "5";
+
+// Comparação frouxa (==)
+if (a == b) {
+    console.log("a == b: São iguais!");
+} else {
+    console.log("a == b: São diferentes!");
+}
+
+// Comparação estrita (===)
+if (a === b) {
+    console.log("a === b: São iguais!");
+} else {
+    console.log("a === b: São diferentes!");
+}
